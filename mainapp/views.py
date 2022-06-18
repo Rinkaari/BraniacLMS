028c5bf32552de122d57706bbeff3148be0e6c2f
Nik Mas <maslow.ns@gmail.com>
2022-06-19 00:47:03 +0300

    lesson_7 memory efficient last 1000 logs

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 import logging
+from collections import deque
 
 from django.conf import settings
 from django.contrib import messages
@@ -11,6 +12,7 @@ from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, TemplateView, UpdateView, View
+from file_read_backwards import FileReadBackwards
 
 from mainapp import forms as mainapp_forms
 from mainapp import models as mainapp_models
@@ -148,13 +150,13 @@ class LogView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(LogView, self).get_context_data(**kwargs)
-        log_slice = []
-        with open(settings.LOG_FILE, "r") as log_file:
+        log_slice = deque()
+        with FileReadBackwards(settings.LOG_FILE, encoding="utf-8") as log_file:
             for i, line in enumerate(log_file):
-                if i == 1000:  # first 1000 lines
+                if i == 1000:  # last 1000 lines
                     break
-                log_slice.insert(0, line)  # append at start
-            context["log"] = "".join(log_slice)
+                log_slice.appendleft(line)  # append at start
+            context["log"] = "\n".join(log_slice)
         return context
 
 
